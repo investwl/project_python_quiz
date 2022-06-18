8f58c1343dd523396f0824281debbfa10ead14ec
investwl <100394537+investwl@users.noreply.github.com>
2022-06-18 19:15:04 +0700

    Update pyquiz_playquiz.py

--- pyquiz_playquiz.py
+++ pyquiz_playquiz.py
@@ -1,21 +1,28 @@
 import psycopg
 
-try:
-    with psycopg.connect(conninfo="postgresql://postgres:angjaya08102005@127.0.0.1:5432/python_quiz") as connection:
-        with connection.cursor() as cursors:
-            def playquiz():
+def playquiz():
+    try:
+        with psycopg.connect(conninfo="postgresql://postgres:angjaya08102005@127.0.0.1:5432/python_quiz") as connection:
+            with connection.cursor() as cursors:
+                question_id = []
+                option_id = []
                 show_question = cursors.execute("select question, question_id, answer from quiz;")
                 take_question = cursors.fetchall()
                 graboption = cursors.execute("select option.question_id, option_label, option_answer from option inner join quiz on option.question_id = quiz.question_id where option.question_id = quiz.question_id;")
                 printoption = cursors.fetchall()
                 point = 0
-                for a in range(len(take_question)):
-                    print("%s. %s"%(a+1, take_question[a][0]))
+                for q_id in range(len(take_question)):
+                    question_id.append(take_question[q_id][1])
+                for o_id in range(len(printoption)):
+                    option_id.append(printoption[o_id][0])
+
+                for main_id in range(len(question_id)):
+                    print("%s. %s"%(main_id+1, take_question[main_id][0]))
                     print("Options :")
-                    for i in range(len(printoption)):
-                        if printoption[i][0] == take_question[a][1]:
-                            print("%s. %s"%(printoption[i][1], printoption[i][2]))
-                    print("===============================")
+                    for checker_id in range(len(option_id)):
+                        if question_id[main_id] == option_id[checker_id]:
+                            print("%s. %s"%(printoption[checker_id][1], printoption[checker_id][2]))
+                    print("=========================")
                     print("Point : %s\n"%(point))
                     while True:
                         useranswer = input("Your answer : ")
@@ -23,7 +30,7 @@ try:
                             print("Only option label is taken as answer.")
                             continue
                         else:
-                            if useranswer == take_question[a][2]:
+                            if useranswer.upper() == take_question[main_id][2]:
                                 print("Correct, you earned a point!")
                                 point += 1
                                 print("Point: %s\n"%(point))
@@ -31,7 +38,9 @@ try:
                                 print("Incorrect! Try better next question.")
                             break
 
-finally:
-    if connection:
-        cursors.close()
-        connection.close()
+                print("End of the game ! Point : %s.\nKeep it up ! See you next game."%(point))
+
+    finally:
+        if connection:
+            cursors.close()
+            connection.close()
