f6c16878ab32738eabfef0ca0de8df5fc8862b5c
investwl <100394537+investwl@users.noreply.github.com>
2022-06-18 19:12:19 +0700

    Update pyquiz_addquestion.py

--- pyquiz_addquestion.py
+++ pyquiz_addquestion.py
@@ -20,27 +20,10 @@ def fill_question(option_library):
         option_library_answer.append(useroption)
     return option_library_answer
 
-def fill_answer(option_library_answer, userquestion, get_user, option_library, question_answer):
-    try:
-        with psycopg.connect(conninfo="postgresql://postgres:angjaya08102005@127.0.0.1:5432/python_quiz") as connection:
-            with connection.cursor() as cursors:
-                for a in range(len(option_library_answer)):
-                    if question_answer.upper() == option_library_answer[a][0]:
-                        insert_table = cursors.execute("insert into quiz(question, answer, user_id) values('%s', '%s', '%s') returning question_id;"%(userquestion, question_answer, get_user))
-                        return_insert = cursors.fetchone()
-                        for b in range(len(option_library_answer)):
-                            cursors.execute("insert into option(question_id, option_label, option_answer) values('%s', '%s', '%s');"%(return_insert[0], option_library[b], option_library_answer[b]))
-                        print("Question successfuly added !\n===================")
-    finally:
-        if connection:
-            cursors.close()
-            connection.close()
-
 def add_question(userlogged):
     try:
         with psycopg.connect(conninfo="postgresql://postgres:angjaya08102005@127.0.0.1:5432/python_quiz") as connection:
             with connection.cursor() as cursors:
-                    
                 while True:
                     opening_quiz = opening_question()
                     if opening_quiz[0] == True:
@@ -54,22 +37,38 @@ def add_question(userlogged):
                             print(fill_quiz)
                             print("Which option is the answer? (only input the ABCs)")
                             question_answer = input("The answer : ")
-                            fill_ans = fill_answer(fill_quiz, opening_quiz[1], get_user[0], option_library, question_answer)
                             break
                         else:
                             print("Minimum 2 options! Example : True or False.")
                             continue
-                    
+
                     print("Add more question?")
                     more_question = input("Yes / No? : ")
                     if more_question.lower() == "no":
                         print("Thanks for adding your questions !")
                         
                         break
-
+                
     finally:
         if connection:
             cursors.close()
             connection.close()
 
-    return fill_quiz, opening_quiz[1], question_answer
+    return fill_quiz, opening_quiz[1], get_user[0], option_library, question_answer.upper()
+    
+
+def fill_answer(option_library_answer, userquestion, get_user, option_library, question_answer):
+    try:
+        with psycopg.connect(conninfo="postgresql://postgres:angjaya08102005@127.0.0.1:5432/python_quiz") as connection:
+            with connection.cursor() as cursors:
+                for a in range(len(option_library_answer)):
+                    if question_answer.upper() == option_library[a][0]:
+                        insert_table = cursors.execute("insert into quiz(question, answer, user_id) values('%s', '%s', '%s') returning question_id;"%(userquestion, question_answer, get_user))
+                        return_insert = cursors.fetchone()
+                        for b in range(len(option_library_answer)):
+                            insert_question = cursors.execute("insert into option(question_id, option_label, option_answer) values('%s', '%s', '%s');"%(return_insert[0], option_library[b], option_library_answer[b]))
+                        print("Question successfuly added !\n===================")
+    finally:
+        if connection:
+            cursors.close()
+            connection.close()
